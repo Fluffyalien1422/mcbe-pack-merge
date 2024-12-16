5587c41cd4f4148e19486fc8a61d8f00bc234b6a
Fluffyalien1422 <shawnbhumbla2@gmail.com>
2024-12-15 17:28:41 -0800

    refactor: move index.ts to merge.ts, export apis from index.ts

--- src/bin.ts
+++ src/bin.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from "@commander-js/extra-typings";
-import { mergePacks } from "./index.js";
+import { mergePacks } from "./merge.js";
 import { Config, configValidator } from "./config.js";
 import * as fs from "fs";
 
